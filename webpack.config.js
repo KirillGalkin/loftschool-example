30271386b83089822874f56e08b5cd209fd81036
Kirill Galkin <galkin@uchi.ru>
2018-01-23 17:09:22 +0300

    vkfilter

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,7 @@ loaders.push({
 
 module.exports = {
     entry: {
-        main: './src/index.js',
-        cookie: './src/cookie.js'
+        main: './src/VKfilter.js'
     },
     output: {
         filename: '[name].[hash].js', // correct
@@ -27,23 +26,11 @@ module.exports = {
         loaders
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({ // correct
-        //     sourceMap: true,
-        //     compress: {
-        //         drop_debugger: false
-        //     }
-        // }),
         new ExtractTextPlugin('styles.css'),
         new HtmlPlugin({
             title: 'Main Homework',
             template: 'index.hbs',
             chunks: ['main']
-        }),
-        new HtmlPlugin({
-            title: 'Div Drag And Drop',
-            template: 'cookie.hbs',
-            filename: 'cookie.html',
-            chunks: ['cookie']
         }),
         new CleanWebpackPlugin(['dist'])
     ]
